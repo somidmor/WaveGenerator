742e4da0a5dbb67fd25dcf3df935fe818cf7765c
somidmor <somidmor@gmail.com>
2023-07-24 15:43:47 -0700

    add FM, reorganize, change AM

--- README.md
+++ README.md
@@ -1,93 +1,48 @@
-# Signal Generator
-#heyyyyyyy changing read me
+# Arduino Waveform Generator
 
+This Arduino code generates a customized waveform based on user inputs. It utilizes the DueTimer library and has been developed for the Arduino Due board. The wave is generated using the DAC0 pin. 
 
+## Features
+* Custom frequency, heights, and portions of waveforms.
+* Capability to generate an indefinite number of waves and blocks of waves.
+* Provides a way to insert a delay between blocks of waves.
+* Ability to add Amplitude Modulation to the waveform.
+* Offers a command to reset the CPU.
+* Possibility to randomly generate the proportions of the wave if not provided by the user.
 
-## Getting started
-
-To make it easy for you to get started with GitLab, here's a list of recommended next steps.
-
-Already a pro? Just edit this README.md and make it your own. Want to make it easy? [Use the template at the bottom](#editing-this-readme)!
-
-## Add your files
-
-- [ ] [Create](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#create-a-file) or [upload](https://docs.gitlab.com/ee/user/project/repository/web_editor.html#upload-a-file) files
-- [ ] [Add files using the command line](https://docs.gitlab.com/ee/gitlab-basics/add-file.html#add-a-file-using-the-command-line) or push an existing Git repository with the following command:
+## User Input Format
+The user input should be in the following format:
 
 ```
-cd existing_repo
-git remote add origin https://gitlab.com/smmirvakili/signal-generator.git
-git branch -M main
-git push -uf origin main
+frequency, height1, portion1, height2, portion2, height3, portion3, height4, portion4, numWaves, delayBetweenBlocks, numBlock, isAM, AMScale
 ```
 
-## Integrate with your tools
+* **frequency**: Frequency of the wave, in the range 1 - 15000 Hz.
+* **heightX**: The height of the wave for the Xth portion, ranging from -1690 to 1690. X can be from 1 to 4.
+* **portionX**: The portion of the wave for the Xth portion, in the range 0 - 10. The sum of all the portions should be equal to 10. X can be from 1 to 4.
+* **numWaves**: The number of waves to generate, in the range 0 - 1000. If it is 0, it will generate waves indefinitely.
+* **delayBetweenBlocks**: The delay between each block of waves, in the range 0 - 1000 ms.
+* **numBlock**: The number of blocks of waves to generate, in the range 0 - 1000. If it is 0, it will generate blocks indefinitely.
+* **isAM**: The amplitude modulation of the wave, in the range 0 - 1690. If it is 0, it will not generate amplitude modulation. If greater than 0 it will generate amplitude modulation, the value is the amplitude of the modulation.
+* **AMScale**: The amplitude scale for modulation. 
 
-- [ ] [Set up project integrations](https://gitlab.com/smmirvakili/signal-generator/-/settings/integrations)
+Note: Portions and heights are for different parts of a wave cycle. You can split a wave cycle into 4 parts and provide heights for each part.
 
-## Collaborate with your team
+## How to Use
+* Upload the script to the Arduino Due board.
+* Open the Serial Monitor.
+* Input the parameters according to the User Input Format mentioned above and press 'Enter'. The Arduino will start generating the waveform.
+* If you want to reset the CPU, type 'r' or 'R' and press 'Enter'.
+* You can input new parameters to generate a new waveform.
 
-- [ ] [Invite team members and collaborators](https://docs.gitlab.com/ee/user/project/members/)
-- [ ] [Create a new merge request](https://docs.gitlab.com/ee/user/project/merge_requests/creating_merge_requests.html)
-- [ ] [Automatically close issues from merge requests](https://docs.gitlab.com/ee/user/project/issues/managing_issues.html#closing-issues-automatically)
-- [ ] [Enable merge request approvals](https://docs.gitlab.com/ee/user/project/merge_requests/approvals/)
-- [ ] [Set auto-merge](https://docs.gitlab.com/ee/user/project/merge_requests/merge_when_pipeline_succeeds.html)
+## Requirements
+* [Arduino Due](https://store.arduino.cc/usa/due.html) or any board supporting analogWriteResolution and DueTimer library.
+* [DueTimer library](https://github.com/ivanseidel/DueTimer)
+  
+## Limitations
+* The actual output waveform will depend on the capabilities of your hardware, including the DAC's resolution and maximum speed.
+* Amplitude Modulation only works on the first portion of the wave.
+* The sum of the user portions should be equal to 10.
+* The portions are integers, so the wave can only be split into parts that are multiples of 10% of the wave.
 
-## Test and Deploy
-
-Use the built-in continuous integration in GitLab.
-
-- [ ] [Get started with GitLab CI/CD](https://docs.gitlab.com/ee/ci/quick_start/index.html)
-- [ ] [Analyze your code for known vulnerabilities with Static Application Security Testing(SAST)](https://docs.gitlab.com/ee/user/application_security/sast/)
-- [ ] [Deploy to Kubernetes, Amazon EC2, or Amazon ECS using Auto Deploy](https://docs.gitlab.com/ee/topics/autodevops/requirements.html)
-- [ ] [Use pull-based deployments for improved Kubernetes management](https://docs.gitlab.com/ee/user/clusters/agent/)
-- [ ] [Set up protected environments](https://docs.gitlab.com/ee/ci/environments/protected_environments.html)
-
-***
-
-# Editing this README
-
-When you're ready to make this README your own, just edit this file and use the handy template below (or feel free to structure it however you want - this is just a starting point!). Thank you to [makeareadme.com](https://www.makeareadme.com/) for this template.
-
-## Suggestions for a good README
-Every project is different, so consider which of these sections apply to yours. The sections used in the template are suggestions for most open source projects. Also keep in mind that while a README can be too long and detailed, too long is better than too short. If you think your README is too long, consider utilizing another form of documentation rather than cutting out information.
-
-## Name
-Choose a self-explaining name for your project.
-
-## Description
-Let people know what your project can do specifically. Provide context and add a link to any reference visitors might be unfamiliar with. A list of Features or a Background subsection can also be added here. If there are alternatives to your project, this is a good place to list differentiating factors.
-
-## Badges
-On some READMEs, you may see small images that convey metadata, such as whether or not all the tests are passing for the project. You can use Shields to add some to your README. Many services also have instructions for adding a badge.
-
-## Visuals
-Depending on what you are making, it can be a good idea to include screenshots or even a video (you'll frequently see GIFs rather than actual videos). Tools like ttygif can help, but check out Asciinema for a more sophisticated method.
-
-## Installation
-Within a particular ecosystem, there may be a common way of installing things, such as using Yarn, NuGet, or Homebrew. However, consider the possibility that whoever is reading your README is a novice and would like more guidance. Listing specific steps helps remove ambiguity and gets people to using your project as quickly as possible. If it only runs in a specific context like a particular programming language version or operating system or has dependencies that have to be installed manually, also add a Requirements subsection.
-
-## Usage
-Use examples liberally, and show the expected output if you can. It's helpful to have inline the smallest example of usage that you can demonstrate, while providing links to more sophisticated examples if they are too long to reasonably include in the README.
-
-## Support
-Tell people where they can go to for help. It can be any combination of an issue tracker, a chat room, an email address, etc.
-
-## Roadmap
-If you have ideas for releases in the future, it is a good idea to list them in the README.
-
-## Contributing
-State if you are open to contributions and what your requirements are for accepting them.
-
-For people who want to make changes to your project, it's helpful to have some documentation on how to get started. Perhaps there is a script that they should run or some environment variables that they need to set. Make these steps explicit. These instructions could also be useful to your future self.
-
-You can also document commands to lint the code or run tests. These steps help to ensure high code quality and reduce the likelihood that the changes inadvertently break something. Having instructions for running tests is especially helpful if it requires external setup, such as starting a Selenium server for testing in a browser.
-
-## Authors and acknowledgment
-Show your appreciation to those who have contributed to the project.
-
-## License
-For open source projects, say how it is licensed.
-
-## Project status
-If you have run out of energy or time for your project, put a note at the top of the README saying that development has slowed down or stopped completely. Someone may choose to fork your project or volunteer to step in as a maintainer or owner, allowing your project to keep going. You can also make an explicit request for maintainers.
+For any other information or clarifications, feel free to open an issue in this repository.
--- waveGenerator/waveGenerator.ino
+++ waveGenerator/waveGenerator.ino
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 
+// user input format:
+// userFrequency: 1-15000, it is the userFrequency of the wave
+// height1: -1690-1690, it is the height of the wave for the first portion
+// portion1: 0-10, it is the portion of the wave for the first portion
+// height2: -1690-1690, it is the height of the wave for the second portion
+// portion2: 0-10, it is the portion of the wave for the second portion
+// height3: -1690-1690, it is the height of the wave for the third portion
+// portion3: 0-10, it is the portion of the wave for the third portion
+// height4: -1690-1690, it is the height of the wave for the fourth portion
+// portion4: 0-10, it is the portion of the wave for the fourth portion
+// numWaves: 0-1000, it is the number of waves to generate, if it is 0, it will generate waves indefinitely
+// delayBetweenBlocks: 0-1000, it is the delay between each block of waves
+// numBlock: 0-1000, it is the number of blocks of waves to generate, if it is 0, it will generate blocks indefinitely
+/* isAM: 0-1690, it is the amplitude modulation of the wave, if it is 0, it will not generate amplitude modulation
+    if greater than 0 it will generate amplitude modulation, the value is the amplitude of the modulation */
+
+
+
+
 enum States {
     INIT,
     GET_USER_INPUT,
@@ -27,21 +46,260 @@ stateStatus gState = {
 static unsigned long cycleCounter = 0;
 static unsigned long waveCounter = 0;
 static double currentHeight = 0;
-int heightLimit = 1690;
+const int heightLimit = 1690;
 
-// int maxHeight; // Maximum height of the user input
-// int minHeight; // Minimum height of the user input
+// Accuracy of the duty cycle in percentage
+const int accuracy = 10;
 
 // User input variables
-int frequency, height1, portion1, height2, portion2, height3, portion3, height4, portion4;
-int numWaves, delayBetweenBlocks, numBlock, shouldAM;
-int accuracy = 10; // Accuracy of the duty cycle in percentage
-bool shouldGoNext = false; // Flag to indicate if the next batch should be started
-String input; // String to store the received input
-int waveType = 1; // Variable to store the type of wave to generate
-int userPortions[4] = {0, 0, 0, 0}; // Store the values of the users portions
-int userHeights[4] = {0, 0, 0, 0}; // Store the values of the users heights
+const int numberOfParams = 16; //number of user parameter
+int userFrequency, height1, portion1, height2, portion2, height3, portion3, height4, portion4;
+int numWaves, delayBetweenBlocks, numBlock, isAM, AMScale, isFM, maxFrequency;
 
+// current frequency of the wave
+int currentFrequency;
+
+// Array to store the heights of the wave
+int cycleWaveHeights[accuracy];
+
+// Flag to indicate if the next batch should be started
+bool shouldGoNext = false;
+
+// String to store the received input
+String input;
+
+// Variable to store the type of wave to generate
+int waveType = 1;
+
+// Store the values of the users portions
+int userPortions[4] = {0, 0, 0, 0};
+
+// Store the values of the users heights
+int userHeights[4] = {0, 0, 0, 0};
+
+
+void resetCounters() {
+  // Reset the counters and stop the timer
+  analogWrite(DAC0, 0); // Set the voltage to 0 between blocks
+  waveCounter = 0;
+  cycleCounter = 0;
+  shouldGoNext = true;
+  Timer1.stop();
+}
+
+
+
+void parseInput(String str) {
+  // Parse the input string and update the user input variables
+  int commaIndex = 0, lastCommaIndex = -1;
+  int portionCount = 0; // Counter to keep track of how many portions have been assigned values
+
+  for (int i = 0; i < numberOfParams; i++) {
+    commaIndex = str.indexOf(',', lastCommaIndex + 1);
+
+    String substr;
+    if (commaIndex == -1) {
+      substr = str.substring(lastCommaIndex + 1);
+    } else {
+      substr = str.substring(lastCommaIndex + 1, commaIndex);
+    }
+
+    int val = substr.toInt();
+
+    switch (i) {
+      case 0: userFrequency = val * accuracy / 2; break;
+      case 1: userHeights[0] = mapVoltage(val); break;
+      case 2: userPortions[0] = val; break;
+      case 3: userHeights[1] = mapVoltage(val); break;
+      case 4: userPortions[1] = val; break;
+      case 5: userHeights[2] = mapVoltage(val); break;
+      case 6: userPortions[2] = val; break;
+      case 7: userHeights[3] = mapVoltage(val); break;
+      case 8: userPortions[3] = val; break;
+      case 9: numWaves = val; break;
+      case 10: delayBetweenBlocks = val; break;
+      case 11: numBlock = val; break;
+      case 12: isAM = val; break;
+      case 13: AMScale = val; break;
+      case 14: isFM = val; break;
+      case 15: maxFrequency = val * accuracy / 2; break;
+    }
+
+    lastCommaIndex = commaIndex;
+  }
+
+  // Assign the portions to the corresponding variables
+  portion1 = userPortions[0];
+  portion2 = userPortions[1];
+  portion3 = userPortions[2];
+  portion4 = userPortions[3];
+  height1 = userHeights[0];
+  height2 = userHeights[1];
+  height3 = userHeights[2];
+  height4 = userHeights[3];
+  currentFrequency = userFrequency;
+
+}
+
+int mapVoltage(int voltage) {
+  // Map the voltage from -1690mV to 1690mV to 0 to 4095
+  return 1.2116*voltage + 2048;
+}
+
+// This function is used to reset the CPU if the user enters 'r' or 'R'
+void shouldResetCPU(){
+    if (Serial.available()) { // If data is available to read
+    input = Serial.readStringUntil('\n'); // Read it until newline
+      if (input == "r" || input == "R") {
+        analogWrite(DAC0, 0);
+        NVIC_SystemReset(); // Reset the CPU
+      }
+  }
+}
+
+// This function is used to set the portions to random values
+void setPortions(int (&portions)[4]){
+  int portionCount = 0;
+  int result[4] = {0, 0, 0, 0}; // Store the values of the portions temporarily
+    // Calculate the sum of the portions that have been set by the user
+  int total = 0;
+  for(int i = 0; i < 4; i++) {
+    if(portions[i] != -1) {
+      total += portions[i];
+      portionCount++;
+    }
+  }
+  // Calculate the sum to be divided among the random portions
+  int randomSum = 10 - total;
+
+  
+  // Assign random values to the portions that were set to -1
+  for(int i = 0; i < 4; i++) {
+    if(portions[i] == -1) {
+      // If there's only one portion left to assign, give it the remaining sum
+      if(4 - portionCount == 1) {
+        result[i] = randomSum;
+      } else {
+        // Otherwise, assign a random value between 1 and the remaining sum
+        result[i] = rand() % randomSum;
+        randomSum -= portions[i];
+      }
+      portionCount++;
+    } else {
+      result[i] = portions[i];
+    }
+  }
+  
+  // Assign the portions to the corresponding variables
+  portion1 = result[0];
+  portion2 = result[1];
+  portion3 = result[2];
+  portion4 = result[3];
+}
+
+// this function is used to generate the amplitude modulation
+void amplitudeModulation() {
+    static int sign = 1;
+    static int highLimit = 4095;
+    static int lowLimit = (AMScale == 0) ? 0 : 2048;
+
+    if (isAM > 0) {
+        height1 += isAM * sign;
+        height1 = (height1 >= highLimit) ? highLimit : height1;
+        height1 = (height1 <= lowLimit) ? lowLimit : height1;
+        sign = (height1 >= highLimit || height1 <= lowLimit) ? -sign : sign;
+        //set the fist portion height of the wave in the next cycle
+        for (int i = 0; i < portion1; i++){
+            cycleWaveHeights[i] = height1;
+            if (AMScale != 0) { // if the user wants to generate amplitude modulation with scale
+            cycleWaveHeights[i + portion1] = lowLimit + ((height1)/AMScale);
+            }
+        }
+    }
+}
+
+// Frequency modulation function
+void frequencyModulation() {
+  static int sign = 1;
+  if (isFM > 0) {
+      currentFrequency += isFM * sign;
+      currentFrequency = (currentFrequency >= maxFrequency) ? maxFrequency : currentFrequency;
+      currentFrequency = (currentFrequency <= userFrequency) ? userFrequency : currentFrequency;
+      sign = (currentFrequency >= maxFrequency || currentFrequency <= userFrequency) ? -sign : sign;
+      Timer1.attachInterrupt(generateBlock).setFrequency(currentFrequency*2).start(); // Update the frequency
+  }
+}
+
+// This function generates a block of the wave
+void generateBlock() {
+  if (cycleCounter >= accuracy) {
+    waveCounter++;
+    frequencyModulation();
+    amplitudeModulation();
+    cycleCounter = 0;
+  }
+
+  // Lookup the height from the precalculated array
+  analogWrite(DAC0, cycleWaveHeights[cycleCounter]);
+
+  cycleCounter++;
+
+  // Reset the counters and stop the timer when the number of waves is reached
+  // if numWaves is 0, the timer will run indefinitely
+  if (waveCounter >= numWaves && numWaves != 0) {
+    resetCounters();
+  }
+}
+
+
+// This function updates the currentHeight based on the current cycleCounter.
+void updateWaveHeight() {
+  for (int i = 0; i < accuracy; i++) {
+    if (i < portion1) {
+      cycleWaveHeights[i] = height1;
+
+    } else if (i < portion1 + portion2) {
+      cycleWaveHeights[i] = height2;
+    } else if (i < portion1 + portion2 + portion3) {
+      cycleWaveHeights[i] = height3;
+    } else if (i < portion1 + portion2 + portion3 + portion4) {
+      cycleWaveHeights[i] = height4;
+    } else {
+      cycleWaveHeights[i] = 0;
+    }
+  }
+  
+}
+
+void generateWaves() {
+
+  updateWaveHeight();
+  
+  // Initialize i outside the loop and the user frequency to the current frequency
+  int i = 0;
+
+  // Use a while loop that runs indefinitely when numBlock is 0
+  while (numBlock == 0 || i < numBlock) {
+    Timer1.attachInterrupt(generateBlock).setFrequency(currentFrequency*2).start(); // Frequency doubled for square wave
+
+    // Wait for the current batch to finish
+    while (!shouldGoNext) {
+      shouldResetCPU();
+    } 
+
+    // Serial.println(i);
+    delay(delayBetweenBlocks);
+    shouldGoNext = false;
+    srand(micros());
+
+    // Only increment i when numBlock is not 0
+    if (numBlock != 0) {
+      i++;
+    }
+  }
+}
+
+ 
 void setup() { 
   Serial.begin(9600); // Begin serial communication at 9600 baud
   analogWriteResolution(12); // Set resolution of the voltage from 0 to 4095
@@ -84,202 +342,4 @@ void loop() {
 
   gState.previous = gState.current;
   gState.current = gState.next;
-}
-
-
-void generateWaves() {
-  for (int i = 0; i < numBlock || numBlock == 0; i++) {
-    Timer1.attachInterrupt(generateBlock).setFrequency(frequency).start(); // Frequency doubled for square wave
-
-    // Wait for the current batch to finish
-    while (!shouldGoNext) {
-      shouldResetCPU();
-    } 
-
-    // Serial.println(i);
-    delay(delayBetweenBlocks);
-    shouldGoNext = false;
-    srand(micros());
-  }
-}
-
-void updateWaveHeight(unsigned long cycleCounter) {
-  // This function updates the currentHeight based on the current cycleCounter.
-  if (cycleCounter < portion1) {
-    currentHeight = height1;
-  } else if (cycleCounter < portion1 + portion2) {
-    currentHeight = height2;
-  } else if (cycleCounter < portion1 + portion2 + portion3) {
-    currentHeight = height3;
-  } else if (cycleCounter < portion1 + portion2 + portion3 + portion4) {
-    currentHeight = height4;
-  } else {
-    currentHeight = 0;
-  }
-}
-
-void amplitudeModulation() {
-    static int sign = 1;
-    if (shouldAM > 0) {
-        height1 += shouldAM * sign;
-        height1 = (height1 >= 4095) ? 4095 : height1;
-        height1 = (height1 <= 0) ? 0 : height1;
-        sign = (height1 >= 4095 || height1 <= 0) ? -sign : sign;
-    }
-}
-
-void resetCounters() {
-  // Reset the counters and stop the timer
-  analogWrite(DAC0, 0);
-  waveCounter = 0;
-  cycleCounter = 0;
-  shouldGoNext = true;
-  Timer1.stop();
-}
-
-void generateBlock() {
-  
-
-  setPortions(userPortions);
-
-  if (waveCounter < numWaves) {
-    if (waveType == 1) {
-      // Normal wave generation
-      updateWaveHeight(cycleCounter);
-    }
-
-    cycleCounter++;
-
-    // Output the current height
-    analogWrite(DAC0, currentHeight);
-
-    
-    if (cycleCounter == accuracy - 1) {
-      waveCounter++;
-      amplitudeModulation();
-      currentHeight = 0;
-    }
-
-    cycleCounter = (cycleCounter + 1) % accuracy;
-
-  } else {
-    resetCounters();
-  }
-}
-
-void parseInput(String str) {
-  // Parse the input string and update the user input variables
-  int commaIndex = 0, lastCommaIndex = -1;
-  int portionCount = 0; // Counter to keep track of how many portions have been assigned values
-
-  for (int i = 0; i < 13; i++) {
-    commaIndex = str.indexOf(',', lastCommaIndex + 1);
-
-    String substr;
-    if (commaIndex == -1) {
-      substr = str.substring(lastCommaIndex + 1);
-    } else {
-      substr = str.substring(lastCommaIndex + 1, commaIndex);
-    }
-
-    int val = substr.toInt();
-
-    switch (i) {
-      case 0: frequency = val * accuracy / 2; break;
-      case 1: userHeights[0] = mapVoltage(val); break;
-      case 2: userPortions[0] = val; break;
-      case 3: userHeights[1] = mapVoltage(val); break;
-      case 4: userPortions[1] = val; break;
-      case 5: userHeights[2] = mapVoltage(val); break;
-      case 6: userPortions[2] = val; break;
-      case 7: userHeights[3] = mapVoltage(val); break;
-      case 8: userPortions[3] = val; break;
-      case 9: numWaves = val; break;
-      case 10: delayBetweenBlocks = val; break;
-      case 11: numBlock = val; break;
-      case 12: shouldAM = val; break;
-    }
-
-    lastCommaIndex = commaIndex;
-  }
-
-  // Calculate the maximum and minimum heights of the user input
-  /**************************************/
-  // maxHeight = userHeights[0];
-  // for(int i = 1; i < 4; i++) {
-  //   if(userHeights[i] > maxHeight) {
-  //     maxHeight = userHeights[i];
-  //   }
-  // }
-
-  // minHeight = userHeights[0];
-  // for(int i = 1; i < 4; i++) {
-  //   if(userHeights[i] < minHeight) {
-  //     minHeight = userHeights[i];
-  //   }
-  // }
-  /**************************************/
-
-  // Assign the portions to the corresponding variables
-  portion1 = userPortions[0];
-  portion2 = userPortions[1];
-  portion3 = userPortions[2];
-  portion4 = userPortions[3];
-  height1 = userHeights[0];
-  height2 = userHeights[1];
-  height3 = userHeights[2];
-  height4 = userHeights[3];
-}
-
-int mapVoltage(int voltage) {
-  // Map the voltage from -1690mV to 1690mV to 0 to 4095
-  return 1.2116*voltage + 2048;
-}
-
-void shouldResetCPU(){
-    if (Serial.available()) { // If data is available to read
-    input = Serial.readStringUntil('\n'); // Read it until newline
-      if (input == "r" || input == "R") {
-        analogWrite(DAC0, 0);
-        NVIC_SystemReset(); // Reset the CPU
-      }
-  }
-}
-
-void setPortions(int (&portions)[4]){
-  int portionCount = 0;
-  int result[4] = {0, 0, 0, 0}; // Store the values of the portions temporarily
-    // Calculate the sum of the portions that have been set by the user
-  int total = 0;
-  for(int i = 0; i < 4; i++) {
-    if(portions[i] != -1) {
-      total += portions[i];
-      portionCount++;
-    }
-  }
-  // Calculate the sum to be divided among the random portions
-  int randomSum = 10 - total;
-
-  
-  // Assign random values to the portions that were set to -1
-  for(int i = 0; i < 4; i++) {
-    if(portions[i] == -1) {
-      // If there's only one portion left to assign, give it the remaining sum
-      if(4 - portionCount == 1) {
-        result[i] = randomSum;
-      } else {
-        // Otherwise, assign a random value between 1 and the remaining sum
-        result[i] = rand() % randomSum;
-        randomSum -= portions[i];
-      }
-      portionCount++;
-    } else {
-      result[i] = portions[i];
-    }
-  }
-  // Assign the portions to the corresponding variables
-  portion1 = result[0];
-  portion2 = result[1];
-  portion3 = result[2];
-  portion4 = result[3]; 
 }
